7087ce1c35b09992329ada4e389586c89b3607f3
PBK-B <PBK9.Bin@gmail.com>
2019-09-07 12:36:53 +0800

    修改 Hello World

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Spin,Icon } from 'antd';
+
+
+const ant_ic = <Icon type="loading" style={{fontSize:50,margin:20}} spin />;
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+		<div>
+			<p>
+			Hello World 6666
+			</p>
+		</div>
+
+		<Spin indicator={ant_ic} />
+
         <a
           className="App-link"
           href="https://reactjs.org"
